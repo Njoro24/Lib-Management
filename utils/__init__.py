26321e5e30d3c2d6ee2fc668e02d8eb8f4fe2662
+ <+>
2025-05-29 11:46:59 +0300

    improved data

---
+++ utils/__init__.py
@@ -0,0 +1,21 @@
+from .validators import (
+    validate_email, 
+    validate_phone, 
+    validate_isbn, 
+    validate_name,
+    get_valid_input,
+    get_yes_no_input, 
+    get_integer_input,
+    get_menu_choice
+)
+
+__all__ = [
+    'validate_email',
+    'validate_phone', 
+    'validate_isbn',
+    'validate_name',
+    'get_valid_input',
+    'get_yes_no_input',
+    'get_integer_input',
+    'get_menu_choice'
+]
